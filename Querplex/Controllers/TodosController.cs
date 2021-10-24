2573be7c9a678dbef2d808f894910b90ff6c1331
Branko Milivojevic <Branko.Milivojevic@bracco.com>
2021-10-24 19:34:39 +0200

    Returnig Status codes with object

--- Querplex/Controllers/TodosController.cs
+++ Querplex/Controllers/TodosController.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using Querplex.Services;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace Querplex.Controllers
 {
@@ -9,7 +11,6 @@ namespace Querplex.Controllers
     [Route("[controller]")]
     public class TodosController : ControllerBase
     {
-
         private readonly ILogger<TodosController> _logger;
         private ITodosService _service;
 
@@ -22,29 +23,50 @@ namespace Querplex.Controllers
         [HttpGet("/api/todos")]
         public ActionResult<List<Todos>> GetTodos()
         {
-            return _service.GetTodos();
+            return StatusCode(StatusCodes.Status200OK, _service.GetTodos());
         }
 
+
         [HttpPost("/api/todos")]
-        public ActionResult<Todos> AddProduct(Todos product)
+        public ActionResult<Todos> AddProduct(Todos todo)
         {
-            _service.AddTodos(product);
-            return product;
+            try
+            {
+                _service.AddTodos(todo);
+                return StatusCode(StatusCodes.Status200OK, todo);
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Auuuch something bad happen {exception}");
+            }
         }
 
         [HttpPut("/api/todos/{id}")]
-        public ActionResult<Todos> UpdateProduct(string id, Todos product)
+        public ActionResult<Todos> UpdateProduct(string id, Todos todo)
         {
-            _service.UpdateTodos(id, product);
-            return product;
+            try
+            {
+                _service.UpdateTodos(id, todo);
+                return StatusCode(StatusCodes.Status200OK, todo);
+            }
+            catch(Exception exception)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Well nice try but you can not update that. {exception}");
+            }
         }
 
         [HttpDelete("/api/todos/{id}")]
         public ActionResult<string> DeleteProduct(string id)
         {
-            _service.DeleteTodos(id);
-            //_logger.LogInformation("products", _products);
-            return id;
+            try
+            {
+                _service.DeleteTodos(id);
+                return StatusCode(StatusCodes.Status200OK, $"Good job you just delete your todo with id: {id}");
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Well you can not delete that todo with id: {id}. It just does not exist Check the exception: {exception}");
+            }
         }
 
     }
--- Querplex/Todos.cs
+++ Querplex/Todos.cs
@@ -8,5 +8,6 @@ namespace Querplex
         public String Name { get; set; }
         public String Company { get; set; }
         public DateTime InterviewDate { get; set; }
+        public bool ContractSign { get; set; }
     }
 }
